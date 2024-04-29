12b2202a9b8cbc4cc414f22afa157e478d1d860f
Marius Miškinis <marius.m@omnisend.com>
2024-04-29 20:30:13 +0300

    Fix indentations

--- Claims.WebApi/Validators/CoverRequestValidator.cs
+++ Claims.WebApi/Validators/CoverRequestValidator.cs
@@ -4,16 +4,16 @@ using FluentValidation;
 namespace Claims.WebApi.Validators
 {
     public class CoverRequestValidator : AbstractValidator<CreateCoverRequest>
-	{
-		public CoverRequestValidator()
-		{
+    {
+        public CoverRequestValidator()
+        {
             RuleFor(request => request.StartDate)
-				.GreaterThanOrEqualTo(DateTime.UtcNow)
-				.WithMessage("StartDate cannot be in the past");
+                .GreaterThanOrEqualTo(DateTime.UtcNow)
+                .WithMessage("StartDate cannot be in the past");
 
             RuleFor(request => request.EndDate)
                 .Must((request, endDate) => endDate <= request.StartDate.AddYears(1))
                 .WithMessage("Total insurance period cannot exceed 1 year");
         }
-	}
+    }
 }
